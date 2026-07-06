1ea0bf0980283c6a29ed379198c4bff603b54e2c
capitainepoof8 <130847632+capitainepoof8@users.noreply.github.com>
2026-07-06 00:56:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,39 +9,76 @@ menu = [
     {"number":6,"name": "Paella", "price": 2500}
 ]
 
-print("\n===========welcome-to-shopy==============")
-
-print("1- afficher le menus du jour")
-print("2- suprimer un plat du menu")
-print("3- liste tous les plas")
-print("4- vide mon panier")
-print("5- affiche la facture")
-print("6- a bientot")
 
 
-choix =input("entrez votre selection (1-6):")
 
-if choix == "1":
-        print("\n============== menus =============")
+
+def ajouter():
+    ajouter = input("\nAjouter au panier (numéro) ou Entrée pour revenir : ")
+    if ajouter.isdigit():
+        
+        trouve = False
+        for plat in menu:
+            if plat['number'] == int(ajouter):
+                panier.append(plat)
+                print(f"-> {plat['name']} ajouté au panier")
+                trouve = True
+               
+        if not trouve:
+            print("Numéro invalide.")
+    
+    
+def afficher_menu(menu):
+    print("=============== menu du jour ==============")
+    if menu:
         for plat in menu:
             print(f"{plat['number']}- {plat['name']}: {plat['price']} FCFA")
-        
-        ajouter = input("Ajouter (numéro): ")
-        if ajouter.isdigit():
-            for plat in menu:
-                if plat['number'] == int(ajouter):
-                    panier.append(plat)
-                    print(f" {plat['name']} ajouté")
-                    break
-                input("\nAppuyez sur Entrer pour continuer...")
-elif choix == "2":
-        print("\n============ PANIER =============")
-        if panier:
-            total = 0
-            for plat in panier:
-                print(f"- {plat['name']}: {plat['price']}")
-                total += plat['price']
-            print(f"TOTAL: {total} FCFA")
         else:
-            print("Panier vide")  
-      
+            print("votre panier est vide")
+        ajouter()
+        return menu
+             
+             
+             
+def suprimer_plat():
+    print("")
+
+def lister_plats(menu):
+    print
+
+
+
+def menu_principal():
+    print("\n===========welcome-to-shopy==============")
+    while True:
+        print("\n1- afficher le menu du jour")
+        print("2- suprimer un plat du menu")
+        print("3- liste tous les plats")
+        print("4- vide mon panier")
+        print("5- affiche la facture")
+        print("6- a bientot")
+ 
+        choix = input("entrez votre selection (1-6): ")
+ 
+        if choix == "1":
+            afficher_menu(menu)
+        elif choix == "2":
+            suprimer_plat(menu)
+        elif choix == "3":
+            lister_plats(menu)
+        elif choix == "4":
+            vider_panier(panier)
+        elif choix == "5":
+            afficher_facture(panier)
+        elif choix == "6":
+            print("\nA bientôt !")
+            break
+        else:
+            print("Choix invalide, entrez un chiffre entre 1 et 6.")
+ 
+def main():
+    
+    menu
+    
+    
+menu_principal()
